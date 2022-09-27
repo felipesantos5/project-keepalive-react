107d14892197f854bcd7974a15a7b042e175faf8
Felipe Santos <99343526+felipesantos5@users.noreply.github.com>
2022-09-27 14:47:17 -0300

    login page finished syles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,28 +2,33 @@ import "./home.scss";
 
 function Home() {
   return (
-    <section>
-      <h1>Olá,</h1>
-      <p className="paragrafo">Para continuar navegando de forma segura, efetue o login na rede.</p>
+    <section className="wrapper">
+      <div className="section-1">
+        <h1>Olá,</h1>
+        <p className="paragrafo">Para continuar navegando de forma segura, efetue o login na rede.</p>
 
-      <h2>Login</h2>
+        <h2>Login</h2>
 
-      <form>
-        <div className="single-input">
-          <input className="input" type="email" id="user" placeholder="Usuário" required/>
-          
-        </div>
+        <form>
+          <div className="wraper-input">
+            <input className="input" type="email" id="user" placeholder="Usuário" required />
 
-        <div className="single-input">
-          <input className="input" type="password" id="password" placeholder="Senha" required />
-        </div>
+            <img className="user icon-input" src="./images/user.svg" alt="usuário" />
+          </div>
 
-        <button>continuar</button>
+          <div className="wraper-input">
+            <input className="input" type="password" id="password" placeholder="Senha" required />
 
-      </form>
+            <img className="locker icon-input" src="./images/locker.svg" alt="cadeado" />
+          </div>
 
-      
+          <button>continuar</button>
+        </form>
+      </div>
 
+      <div className="section-2">
+        <img className="logo-compass" src="./images/LogoCompasso.svg" alt="Logo Compass" />
+      </div>
     </section>
   );
 }
