c75b74c027233fd5d6e0dd5c883719182b1cabfd
Felipe Santos <99343526+felipesantos5@users.noreply.github.com>
2022-10-10 16:56:39 -0300

    feat: create page register

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,18 @@
+ function Register() {
+  return (
+    <div>
+      
+      <h1>teste</h1>
+      <input type="text" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque quo, porro necessitatibus doloribus quod ab soluta consequatur laborum vitae dignissimos totam iure unde molestias. Adipisci
+        a non inventore consequuntur!
+      </p>
+      <div>
+        <h2>title</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
