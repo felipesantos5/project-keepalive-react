294017c60154275a851fecb7d893488a222680c2
Felipe Santos <99343526+felipesantos5@users.noreply.github.com>
2022-10-10 16:55:12 -0300

    add route register

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function Router() {
   return (
@@ -8,6 +9,7 @@ function Router() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
