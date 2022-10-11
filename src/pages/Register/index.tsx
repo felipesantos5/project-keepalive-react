1e4b749de4e1a40c8c896cba6f36dafecbf0b4c8
Felipe Santos <99343526+felipesantos5@users.noreply.github.com>
2022-10-11 03:03:02 -0300

    refactor: componentized the page

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,17 +1,44 @@
- function Register() {
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/button/button";
+import Input from "../../components/input/input";
+import TextIntroduction from "../../components/text-introduction/textIntruduction";
+import "./register.scss";
+
+function Register() {
+  const navigate = useNavigate();
+
+  // const validateRegister = () {
+
+  // }
+
+  function navigateLogin () {
+    navigate("/");
+  }
+  
   return (
-    <div>
-      
-      <h1>teste</h1>
-      <input type="text" />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque quo, porro necessitatibus doloribus quod ab soluta consequatur laborum vitae dignissimos totam iure unde molestias. Adipisci
-        a non inventore consequuntur!
-      </p>
-      <div>
-        <h2>title</h2>
+    <section className="wrapper-sec1">
+      <div className="section-1">
+        <TextIntroduction textParagraph="Para continuar navegando de forma segura, efetue o registro na rede." textTitle="Registro"></TextIntroduction>
+
+        <Input Type="email" label="Email" icon="fa fa-check"></Input>
+
+          <Input Type="text" label="Nome" icon="fa fa-check"></Input>
+          <Input Type="number" label="Sobrenome" icon="fa fa-check"></Input>
+
+          <Input Type="password" label="Senha" icon="fa fa-check"></Input>
+          <Input Type="password" label="Repita sua senha" icon="fa fa-check"></Input>
+
+          {/* {emailErr && <p className="error-input error-active">Ops, usuário ou senha inválidos. Tente novamente!</p>} */}
+
+        <Button label="inscreva-se"></Button>
+        
+        <a className="anchor-redirect" onClick={navigateLogin}>se você já possui um cadastro clique aqui</a>
       </div>
-    </div>
+
+      <div className="section-2">
+        <img className="logo-compass-sec2" src="./images/LogoCompasso.svg" alt="Logo Compass" />
+      </div>
+    </section>
   );
 }
 
